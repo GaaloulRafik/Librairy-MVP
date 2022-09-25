6553b0c9da6d40f77472ee67753516a6836c64a3
unknown <gaaloul.rafik@yahoo.fr>
2022-09-25 23:23:25 +0100

    first update

---
+++ LibraryMVP/LibraryMVP/Models/BooksDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVP.Models
+{
+    class BooksDataModel
+    {
+        public int Book_ID { get; set; }
+        public string Book_Name { get; set; }
+        public string Book_Author { get; set; }
+        public int Book_PageNumber { get; set; }
+
+    }
+}
